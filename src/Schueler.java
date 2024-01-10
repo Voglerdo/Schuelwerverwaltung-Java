17a05c7204ebe7b4459585a5079bd013598436e5
Voglerdo <vogler.dominikq11@gmail.com>
2024-01-10 11:15:26 +0100

    Initial commit

---
+++ src/Schueler.java
@@ -0,0 +1,9 @@
+public class Schueler {
+    String name;
+    String nachname ;
+
+    public Schueler(String name, String nachname) {
+        this.name = name;
+        this.nachname = nachname;
+    }
+}
